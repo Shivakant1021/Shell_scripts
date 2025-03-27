ea1c204c0d1623b6ffe9c5a1bf6b072461cdb936
Shivakant1021 <shivakant011999@gmail.com>
2025-03-27 16:07:45 +0000

    reading from user

---
+++ 03_read.sh
@@ -0,0 +1,30 @@
+#!bin/bash
+#readin from user
+
+
+
+echo "What is your name"
+
+read name
+
+echo "my name is $name"
+
+
+echo "Enter first Number:" 
+read number1
+echo "Enter Secont Number:" 
+read number2
+
+sum=$(( $number1 + $number2 ))
+
+echo "Addition of Two Number is: $sum" 
+
+
+
+
+
+
+
+
+
+
