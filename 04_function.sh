b15e0a9cb159f7383da712ee00b366f549481e2d
Shivakant1021 <shivakant011999@gmail.com>
2025-03-28 16:50:38 +0000

    functions in scripts

---
+++ 04_function.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#In this script working on functions 
+
+
+echo "this function is for additio"
+function addition(){
+	let a=10
+	let b=20
+	sum=$((a + b ))
+	echo "the addition of two number is: $sum"
+}
+
+addition
+
+
+
+
+
+
+
+
+
