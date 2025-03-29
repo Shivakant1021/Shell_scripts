c449114319de3748b2cfd3f0a449206f9d89c946
Shivakant1021 <shivakant011999@gmail.com>
2025-03-29 06:12:11 +0000

    script 7 for ping

---
+++ 07_pingforwebsite.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#For checking status of websites 
+
+read -p "which website you want to check:" site
+
+ping -c 1 $site
+
+#sleep 1s 
+
+if [[ $? -eq 0 ]]
+then
+	echo "Successfully conntected to site"
+else
+	echo "unable to connect $site"
+fi
+
+
+
+
+
