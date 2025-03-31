b6f0cd1038cd48f942bf5a03045ddcdc66ba075d
Shivakant1021 <shivakant011999@gmail.com>
2025-03-31 08:11:22 +0000

    Server file changes

--- my_project/myapp/server.js
+++ my_project/myapp/server.js
@@ -1,5 +1,35 @@
 const http = require("http");
-http.createServer((req, res) => {
-    res.write("Hello, World!");
-    res.end();
-}).listen(3000, () => console.log("Server running on port 3000"));
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        // Serve HTML file
+        fs.readFile("index.html", (err, data) => {
+            if (err) {
+                res.writeHead(500, { "Content-Type": "text/plain" });
+                res.end("Internal Server Error");
+            } else {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            }
+        });
+    } else if (req.url === "/style.css") {
+        // Serve CSS file
+        fs.readFile("style.css", (err, data) => {
+            if (err) {
+                res.writeHead(404, { "Content-Type": "text/plain" });
+                res.end("CSS file not found");
+            } else {
+                res.writeHead(200, { "Content-Type": "text/css" });
+                res.end(data);
+            }
+        });
+    } else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Page not found");
+    }
+});
+
+server.listen(3000, () => console.log("Server running on port 3000"));
+
