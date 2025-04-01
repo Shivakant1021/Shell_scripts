17fe9b1f9ef3640945d61cd61babcbc0af00c0b9
Shivakant1021 <shivakant011999@gmail.com>
2025-04-01 19:44:31 +0000

    modified myapp

--- my_project/myapp/server.js
+++ my_project/myapp/server.js
@@ -3,33 +3,24 @@ const fs = require("fs");
 const path = require("path");
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        // Serve HTML file
-        fs.readFile("index.html", (err, data) => {
-            if (err) {
-                res.writeHead(500, { "Content-Type": "text/plain" });
-                res.end("Internal Server Error");
-            } else {
-                res.writeHead(200, { "Content-Type": "text/html" });
-                res.end(data);
-            }
-        });
-    } else if (req.url === "/style.css") {
-        // Serve CSS file
-        fs.readFile("style.css", (err, data) => {
-            if (err) {
-                res.writeHead(404, { "Content-Type": "text/plain" });
-                res.end("CSS file not found");
-            } else {
-                res.writeHead(200, { "Content-Type": "text/css" });
-                res.end(data);
-            }
-        });
-    } else {
-        res.writeHead(404, { "Content-Type": "text/plain" });
-        res.end("Page not found");
-    }
+    let filePath = path.join(__dirname, "public", req.url === "/" ? "index.html" : req.url);
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("404 Not Found");
+        } else {
+            let ext = path.extname(filePath);
+            let contentType = "text/html";
+
+            if (ext === ".css") contentType = "text/css";
+
+            res.writeHead(200, { "Content-Type": contentType });
+            res.end(data);
+        }
+    });
 });
 
 server.listen(3000, () => console.log("Server running on port 3000"));
+
 
